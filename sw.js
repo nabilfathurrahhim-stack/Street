75830be754789e81848d0d2be2a061efaaf315a0
nabilfathurrahhim-stack <nabilfathurrahhim@gmail.com>
2026-06-04 12:04:50 +0700

    Update service worker to use new cache version and assets

--- sw.js
+++ sw.js
@@ -1,21 +1,28 @@
-const CACHE_NAME = 'blueride-v2';
-const assets = [
+const CACHE_NAME = 'blueride-v3';
+const ASSETS = [
+  './',
   './index.html',
-  './manifest.json'
+  './manifest.json',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
+  'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
+self.addEventListener('install', event => {
+  event.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(assets);
-    })
+      return cache.addAll(ASSETS);
+    }).then(() => self.skipWaiting())
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
     })
   );
 });
